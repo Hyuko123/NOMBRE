e6baad3ce7d0dd2079987cee2db62d40726d45c5
Hyuko123 <gaylordlemene@gmail.com>
2025-12-25 04:15:14 +0100

    Delete interactions directory

--- interactions/buttons.js
+++
@@ -1,9 +0,0 @@
-module.exports = client => {
-  client.on("interactionCreate", async interaction => {
-    if (!interaction.isButton()) return;
-
-    if (interaction.customId === "close") {
-      await interaction.channel.delete().catch(() => {});
-    }
-  });
-};
