79145b2e545d59d109497079cc79e8b7b6326bf5
Hyuko123 <gaylordlemene@gmail.com>
2025-12-25 04:15:21 +0100

    Delete systems directory

--- systems/memberCount.js
+++
@@ -1,9 +0,0 @@
-const { MEMBER_COUNT_CHANNEL_ID } = require("../config/channels");
-
-async function updateMemberCount(guild) {
-  const channel = guild.channels.cache.get(MEMBER_COUNT_CHANNEL_ID);
-  if (!channel) return;
-  channel.setName(`👥 Membres : ${guild.memberCount}`).catch(() => {});
-}
-
-module.exports = { updateMemberCount };
